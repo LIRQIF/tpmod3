7cbb4f49ff1f5bbd9097849aa1c579eb9504db79
fiqril <mfikrilh@gmail.com>
2022-03-11 07:17:56 -0800

    commit ke dua

---
+++ tpmodul3_1302201133/KodePos.cs
@@ -0,0 +1,54 @@
+﻿public enum Kelurahan
+{
+    Batununggal,
+    Kujangsari,
+    Mengger,
+    Wates,
+    Cijaura,
+    Jatisari,
+    Margasari,
+    Sekejati,
+    Kebonwaru,
+    Maleer,
+    Samoja,
+}
+
+public class KodePos
+{
+    public Kelurahan kelurahan;
+
+    public KodePos(Kelurahan kelurahan)
+    {
+        this.kelurahan = kelurahan;
+    }
+
+    public String getKodePos()
+    {
+        switch (this.kelurahan)
+        {
+            case Kelurahan.Batununggal:
+                return "40266";
+            case Kelurahan.Kujangsari:
+                return "40287";
+            case Kelurahan.Mengger:
+                return "40267";
+            case Kelurahan.Wates:
+                return "40256";
+            case Kelurahan.Cijaura:
+                return "40287";
+            case Kelurahan.Jatisari:
+                return "40286";
+            case Kelurahan.Margasari:
+                return "40286";
+            case Kelurahan.Sekejati:
+                return "40286";
+            case Kelurahan.Kebonwaru:
+                return "40272";
+            case Kelurahan.Maleer:
+                return "40274";
+            default:
+                return "40273";
+        }
+
+    }
+}
--- tpmodul3_1302201133/Program.cs
+++ tpmodul3_1302201133/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var kelurahan = new KodePos(Kelurahan.Kebonwaru);
+            Console.WriteLine($"Nama Kelurahan\t\t: {kelurahan.kelurahan}");
+            Console.WriteLine($"Kode Pos Kelurahan\t: {kelurahan.getKodePos()}");
+        }
+    }
+}
