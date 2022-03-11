afb883eba01ed2e06c325f1b5596ce9ea270faac
fiqril <mfikrilh@gmail.com>
2022-03-11 06:57:28 -0800

    commit pertama

---
+++ README.md
@@ -0,0 +1,1 @@
+# tpmod3kpl
---
+++ tpmodul3_1302201133/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var kelurahan = new KodePos(Kelurahan.Kebonwaru);
+            Console.WriteLine($"Nama Kelurahan\t\t: {kelurahan.kelurahan}");
+            Console.WriteLine($"Kode Pos Kelurahan\t: {kelurahan.getKodePos()}");
+
+            var doorMachine = new DoorMachine();
+            doorMachine.printStatus();
+            doorMachine.setState(State.Terbuka);
+            doorMachine.printStatus();
+        }
+    }
+}
