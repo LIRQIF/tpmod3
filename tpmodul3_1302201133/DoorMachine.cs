25a8002d71539807eb8fa23c02ce7683cc12f399
fiqril <mfikrilh@gmail.com>
2022-03-11 07:48:27 -0800

    commit ke tiga

---
+++ tpmodul3_1302201133/DoorMachine.cs
@@ -0,0 +1,28 @@
+﻿public enum State
+{
+    Terkunci,
+    Terbuka,
+}
+
+public class DoorMachine
+{
+    private State state = State.Terkunci;
+
+    public void setState(State state)
+    {
+        this.state = state;
+    }
+
+    public void printStatus()
+    {
+        switch (this.state)
+        {
+            case State.Terkunci:
+                Console.WriteLine("Pintu Terkunci");
+                break;
+            case State.Terbuka:
+                Console.WriteLine("Pintu Tidak Terkunci");
+                break;
+        }
+    }
+}
--- tpmodul3_1302201133/Program.cs
+++ tpmodul3_1302201133/Program.cs
@@ -9,6 +9,11 @@ namespace MyApp
             var kelurahan = new KodePos(Kelurahan.Kebonwaru);
             Console.WriteLine($"Nama Kelurahan\t\t: {kelurahan.kelurahan}");
             Console.WriteLine($"Kode Pos Kelurahan\t: {kelurahan.getKodePos()}");
+
+            var doorMachine = new DoorMachine();
+            doorMachine.printStatus();
+            doorMachine.setState(State.Terbuka);
+            doorMachine.printStatus();
         }
     }
 }
